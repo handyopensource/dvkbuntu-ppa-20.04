fd7746a5a4d8ddc0f995ca695fb8a12b6995d0a8
Paullux <paulwoisard@gmail.com>
2020-06-16 11:19:42 +0200

    Mise de cubic et theme light

---
+++ dvkbuntu-plasma-theme-light/look-and-feel/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,253 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "687",
+                    "DialogWidth": "1200"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                },
+                "/General": {
+                    "ToolBoxButtonState": "topcenter",
+                    "ToolBoxButtonX": "346",
+                    "ToolBoxButtonY": "70",
+                    "pressToMoveHelp": "false",
+                    "sortMode": "-1"
+                },
+                "/Wallpaper/org.kde.color/General": {
+                    "Color": "0,0,255"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///usr/share/wallpapers/DVKBUNTU-Light/contents/images/1920x1080.png"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "2",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "PreloadWeight": "0",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/ConfigDialog": {
+                            "DialogHeight": "680",
+                            "DialogWidth": "1360"
+                        },
+                        "/Configuration/Configuration/Configuration": {
+                            "PreloadWeight": "34",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration": {
+                            "PreloadWeight": "42",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration/Configuration": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration/Configuration/Configuration/General": {
+                            "launcherUrls": "file:///usr/share/applications/DVKBuntuMenuAccueil.desktop"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration/Configuration/General": {
+                            "launcherUrls": "file:///usr/share/applications/DVKBuntuMenuAccueil.desktop"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration/General": {
+                            "launcherUrls": "file:///usr/share/applications/DVKBuntuMenuAccueil.desktop"
+                        },
+                        "/Configuration/Configuration/Configuration/General": {
+                            "launcherUrls": "file:///usr/share/applications/DVKBuntuMenuAccueil.desktop"
+                        },
+                        "/Configuration/Configuration/General": {
+                            "launcherUrls": "file:///usr/share/applications/DVKBuntuMenuAccueil.desktop"
+                        },
+                        "/Configuration/General": {
+                            "launcherUrls": "file:///usr/share/applications/DVKBuntuMenuAccueil.desktop"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.quicklaunch"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "2",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "PreloadWeight": "0",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration": {
+                            "PreloadWeight": "34",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration": {
+                            "PreloadWeight": "42",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration/Configuration": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "2",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "PreloadWeight": "0",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration": {
+                            "PreloadWeight": "34",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration": {
+                            "PreloadWeight": "42",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration/Configuration": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.taskmanager"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "2",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "PreloadWeight": "0",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/ConfigDialog": {
+                            "DialogHeight": "680",
+                            "DialogWidth": "1360"
+                        },
+                        "/Configuration/Configuration/Configuration/General": {
+                            "show_lockScreen": "false"
+                        },
+                        "/Configuration/Configuration/General": {
+                            "show_lockScreen": "false"
+                        },
+                        "/Configuration/General": {
+                            "show_lockScreen": "false"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.lock_logout"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "2",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "PreloadWeight": "0",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration": {
+                            "PreloadWeight": "34",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration": {
+                            "PreloadWeight": "42",
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/Configuration/Configuration": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock2"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "42"
+                        },
+                        "/Configuration/General": {
+                            "length": "19"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "93",
+                    "DialogWidth": "1920"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                }
+            },
+            "height": 2.3333333333333335,
+            "hiding": "normal",
+            "location": "top",
+            "maximumLength": 64,
+            "minimumLength": 64,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
